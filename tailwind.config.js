aaff354ec0e2e33167ca2893cfeb7bc0ef94cd36
Hendrik Neumann <henne@armselig.net>
2021-01-31 21:46:48 +0100

    build(tailwind): configure purging

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 // const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: [],
+  purge: [
+    './assets/**/*.{css,scss}',
+    './components/**/*.{js,jsx,ts,tsx,vue}',
+    './layouts/**/*.{js,jsx,ts,tsx,vue}',
+    './pages/**/*.{js,jsx,ts,tsx,vue}',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: { primary: '#123456' },
