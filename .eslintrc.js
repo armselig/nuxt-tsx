fb7ee0cd2d903f721001f8714311c814a02b7b58
Hendrik Neumann <henne@armselig.net>
2021-01-31 22:50:16 +0100

    build(linting): disable eslint rules that are problematic with TS

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,10 @@ module.exports = {
   plugins: ['prettier', 'eslint-plugin-tsdoc'],
   // add your custom rules here
   rules: {
-    '@typescript-eslint/no-unused-vars-experimental': true,
-    'tsdoc/syntax': 'warn',
+    '@typescript-eslint/no-unused-vars': 0, // See: https://github.com/typescript-eslint/typescript-eslint/issues/1856
+    '@typescript-eslint/no-unused-vars-experimental': 1,
+    'no-undef': 0, // See: https://github.com/typescript-eslint/typescript-eslint/issues/342
+    'no-unused-vars': 0,
+    'tsdoc/syntax': 1,
   },
 }
